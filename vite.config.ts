41ebb7acb3e25a47653e680fff421866e999a71f
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-10-07 22:08:08 +0300

    chore(seo): added google analytics
    * used vite-plugin-radar for google analytics & gtm integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import Vue from '@vitejs/plugin-vue';
+import ViteRadar from 'vite-plugin-radar';
 import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		vue(),
+		// Import Vue
+		Vue(),
+		// Import Windi CSS
 		WindiCSS({
 			config: './windi.config.ts',
+		}),
+		// Import Radar (analytics)
+		ViteRadar({
+			enableDev: true,
+			analytics: [{ id: 'G-6Q9NPHELLL' }],
+			gtm: [{ id: 'G-6Q9NPHELLL' }],
 		}),
 	],
 });
